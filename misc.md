26762028ef3948dd60d7fafce608d23809494a07
sidnix001 <vmakisev65@gmail.com>
2025-06-10 22:08:09 +0300

    changes #1

--- misc.md
+++ misc.md
@@ -1,8 +1,6 @@
-- website color palette:
+website color palette:
 #26547C
 #EF476F
 #FFD166
 #06d6a0
 #FCFCFC
-
-**hey, this text commits and push from vs code :D !!!**
--- script.js
+++ script.js
@@ -4,15 +4,16 @@ let isFirstVisit = !Boolean(localStorage.getItem("isFirstVisit"));
 console.log(isFirstVisit);
 
 if (!isFirstVisit) {
-    $("#welcome-screen").hide();
+    $("#welcome-screen-container").hide();
 }
 
 $(document).ready(() => {
+    // --- welcome screen ---
     if (isFirstVisit) {
         $("#welcome-screen-start-btn").click(() => {
             localStorage.setItem("isFirstVisit", "false");
-            $("#welcome-screen").animate({opacity: 0}, 100, () => {
-                $("#welcome-screen").hide();
+            $("#welcome-screen-container").animate({opacity: 0}, 100, () => {
+                $("#welcome-screen-container").hide();
 
                 setTimeout(() => {
                     $("#app").show(() => {
@@ -26,4 +27,18 @@ $(document).ready(() => {
             $("#app").animate({opacity: 1}, 200, ()=>{});
         });
     }
+
+    // --- sidebar ---
+    let isSideBarOpen = false
+     $("#burger-menu-icon").click(() => {
+        isSideBarOpen = !isSideBarOpen;
+        if (isSideBarOpen) {
+            $(".sidebar").animate({width: "30vw"}, 400, () => {})
+            setTimeout(() => {
+                $(".content").show()
+            }, 200);
+        } else {
+            $(".sidebar").animate({width: "0"}, 400, () => {$(".content").hide()})
+        }
+     })
 });
